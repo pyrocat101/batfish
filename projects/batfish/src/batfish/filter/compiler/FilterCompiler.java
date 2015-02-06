ca07304dd00c7aed1c8497ea1a0dcaf45c9024ec
Linjie Ding <i@pyroc.at>
2015-02-05 20:53:42 -0800

    refactored filter compiler

--- projects/batfish/src/batfish/filter/compiler/FilterCompiler.java
+++ projects/batfish/src/batfish/filter/compiler/FilterCompiler.java
@@ -3,14 +3,28 @@ package batfish.filter.compiler;
 import batfish.filter.ast.node.*;
 import batfish.z3.node.*;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class FilterCompiler {
-    private static SrcIDCompiler srcIDCompiler = new SrcIDCompiler();
-    private static DstIDCompiler dstIDCompiler = new DstIDCompiler();
+    private static final SrcIDCompiler srcIDCompiler = new SrcIDCompiler();
+    private static final DstIDCompiler dstIDCompiler = new DstIDCompiler();
 
-    public static VarIntExpr IP_PROT = new VarIntExpr("ip_prot");
-    public static int IP_PROT_NBIT = 9;
+    public static final VarIntExpr IP_PROT_VAR = new VarIntExpr("ip_prot");
+    public static final int IP_PROT_NBIT = 9;
+
+    // IP protocol numbers:
+    // ICMP - 1
+    // IGMP - 2
+    // TCP - 6
+    // UDP - 17
+    // SCTP - 132
+    public static final int PROTO_ICMP = 1;
+    public static final int PROTO_IGMP = 2;
+    public static final int PROTO_TCP = 6;
+    public static final int PROTO_UDP = 17;
+    public static final int PROTO_SCTP = 132;
 
     public static LitIntExpr protoNumber(int num) {
         return new LitIntExpr(num, IP_PROT_NBIT);
@@ -20,34 +34,33 @@ public class FilterCompiler {
      * compile({proto, dir, type}) = compile(proto) && compile(dir, type)
      */
     public static BooleanExpr compile(Filter filter) {
-        BooleanExpr proto = compile(filter.protocol);
-        BooleanExpr id = compile(filter.direction, filter.id);
-        return new AndExpr(Arrays.asList(proto, id));
+        List<BooleanExpr> terms = new ArrayList<BooleanExpr>();
+        if (filter.protocol != null) {
+            terms.add(compile(filter.protocol));
+        }
+        if (filter.id != null) {
+            terms.add(compile(filter.direction, filter.id));
+        }
+        return new AndExpr(terms).simplify();
     }
 
     /**
      * compile(proto) = "(= ip_prot {proto_number})"
      */
     public static BooleanExpr compile(Protocol protocol) {
-        // IP protocol numbers:
-        // ICMP - 1
-        // IGMP - 2
-        // TCP - 6
-        // UDP - 17
-        // SCTP - 132
         switch (protocol) {
             case IP:
                 return TrueExpr.INSTANCE;
             case ICMP:
-                return new EqExpr(IP_PROT, protoNumber(1));
+                return new EqExpr(IP_PROT_VAR, protoNumber(PROTO_ICMP));
             case IGMP:
-                return new EqExpr(IP_PROT, protoNumber(2));
+                return new EqExpr(IP_PROT_VAR, protoNumber(PROTO_IGMP));
             case TCP:
-                return new EqExpr(IP_PROT, protoNumber(6));
+                return new EqExpr(IP_PROT_VAR, protoNumber(PROTO_TCP));
             case UDP:
-                return new EqExpr(IP_PROT, protoNumber(17));
+                return new EqExpr(IP_PROT_VAR, protoNumber(PROTO_UDP));
             case SCTP:
-                return new EqExpr(IP_PROT, protoNumber(132));
+                return new EqExpr(IP_PROT_VAR, protoNumber(PROTO_SCTP));
         }
         // make IDE happy
         return null;
